026db1aaff2e0487d821048e425050dd0abf6395
jessi25924 <jessievirtucio>
2025-07-25 11:40:07 +0100

    Add toast notification

--- floral_fantasy/settings.py
+++ floral_fantasy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0n%ftd-%mnpay=%3oei%ydar(bv322=mc_g%x$dej1r*qw4$=l'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -82,6 +82,8 @@ TEMPLATES = [
         },
     },
 ]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
