d67da9ed50254d01bbb6db2a62fde53340efa5f8
jessi25924 <jessievirtucio>
2025-07-31 09:15:18 +0100

    Initial deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn floral_fantasy.wsgi:application
--- floral_fantasy/settings.py
+++ floral_fantasy/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com', 
+    '127.0.0.1',
+    'localhost',
+    ]
 
 
 # Application definition
