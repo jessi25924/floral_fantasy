9ac9321d3075271ecea669a5d86642327c6f57f3
jessi25924 <jessievirtucio>
2025-08-21 21:06:53 +0100

    Add edit product functionality

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('add/', views.add_product, name='add_product'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
