fc8c2afef1a0edcfa15dec803bccd2f699c3c1d0
jessi25924 <jessievirtucio>
2025-08-25 13:46:39 +0100

    contact form prod email

--- floral_fantasy/settings.py
+++ floral_fantasy/settings.py
@@ -223,6 +223,7 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL') # for production
+    CONTACT_RECIPIENT_EMAIL = "floralfantasyexample@gmail.com"
     # password reset sent only on terminal
     # DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', os.environ.get('EMAIL_HOST_USER'))
 
